d9ab095fa3309794f0d5483a4d336217aaaa78e6
clins210 <72818070+clins210@users.noreply.github.com>
2021-11-20 18:38:46 +0800

    UI update

--- frontend/src/Component/NearStation.js
+++ frontend/src/Component/NearStation.js
@@ -3,45 +3,6 @@ import 'antd/dist/antd.css';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 
-
-const options = [
-  {
-    value: '淡水',
-    label: '淡水',
-    children: [
-        {
-            value: '淡水',
-            label: '淡水',
-        },
-        {
-            value: '劍潭',
-            label: '劍潭',
-        },
-        {
-            value: '石牌',
-            label: '石牌',
-        },
-        {
-            value: '明德',
-            label: '明德',
-        },
-        ],
-  },{
-  value: '板南',
-  label: '板南',
-  children: [
-      {
-          value: '市政府',
-          label: '市政府',
-      },
-      {
-          value: '國父紀念館',
-          label: '國父紀念館',
-      }
-      ],
-},
-];
-
 const NearStation = ({setStation}) => {
 
     const [stationList, setStationList] = useState([])
@@ -64,10 +25,9 @@ const NearStation = ({setStation}) => {
         <Cascader  
             // size="large"
             style={{ width: '100%' }}
-            defaultValue={['板南', '市政府']}
+            defaultValue={['松山新店', '公館']}
             options={stationList}
             onChange={handleChange}
-            placeholder="最靠近的捷運站："
         />
     )
 }
