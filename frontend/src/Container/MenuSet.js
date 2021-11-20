51b53b6a3c0f48d2a8ec33b8b8c687b0faa8af34
clins210 <72818070+clins210@users.noreply.github.com>
2021-11-20 18:38:12 +0800

    UI upgate

--- frontend/src/Container/MenuSet.js
+++ frontend/src/Container/MenuSet.js
@@ -1,5 +1,5 @@
 import {useState, useEffect } from 'react';
-import { CarOutlined, SwapOutlined, HistoryOutlined, TagsOutlined, SearchOutlined  } from '@ant-design/icons';
+import { CarOutlined, SwapOutlined, HistoryOutlined, TagsOutlined, SearchOutlined, HomeOutlined  } from '@ant-design/icons';
 import { Button } from 'antd';
 import axios from 'axios';
 import spotnext from '../spotnext.svg';
@@ -7,13 +7,12 @@ import '../App.css'
 import SuggestList from '../Component/SuggestList';
 import TimeSelect from '../Component/TimeSelect';
 import TagSelect from '../Component/TagSelect';
-import NearStation from '../Component/NearStation'
-import TransferSelect from '../Component/TransferSelect'
-
+import NearStation from '../Component/NearStation';
+import TransferSelect from '../Component/TransferSelect';
 
 const MenuSet = () => {
-    const [station, setStation] = useState(["板南","市政府"])
-    const [time, setTime] = useState(10)
+    const [station, setStation] = useState(['松山新店', '公館'])
+    const [time, setTime] = useState(0)
     const [transfer, setTransfer] = useState(false)
     const [tag, setTag] = useState([])
     const [check, setCheck] = useState(false)
@@ -21,23 +20,6 @@ const MenuSet = () => {
     const [returnData, setReturnData] = useState([])
 
     useEffect(() => {
-        // fetch("db.php")
-        fetch('http://192.168.50.214/db.php')
-        .then((res) => { 
-          console.log("db.php api ok");
-          console.log(res)
-        })
-        .catch((error) => { console.log(error); console.log('DB/PHP/QQ') })
-      },[])
-
-    useEffect(() => {
-        let userInput = {
-            "nearStation": station,
-            "time": time,
-            "transfer": transfer,	
-            "tag": tag
-        }
-
         axios.post('http://127.0.0.1:8000/api/spots/search', {
           "nearStation":station,
           "time":time,
@@ -46,7 +28,6 @@ const MenuSet = () => {
         })
         .then((res) => { 
           setReturnData(res.data);
-          console.log(userInput);
           console.log("spots/search api ok");
         })
         .catch((error) => { console.log(error); console.log('api/spots/search~~~GG') })
@@ -56,13 +37,30 @@ const MenuSet = () => {
         if (tag.length===0) {
             alert("請選取景點類別")
         }else{
+
             setCheck(!check)
-            setShow(true)
+            setShow(!show)
         }
+    }
+    const handelBack = () => {
+        // restart
+        setTag([])
+        setTime(10)
+        setTransfer(false)
+        setStation(['松山新店', '公館'])
+        setReturnData([])
+        setCheck(false)
+        setShow(false)
+
     }
     const Btm =() =>{
         return(
             <Button onClick={()=>handelCheck()} type="default" shape="round" icon={<SearchOutlined />} size="large">查詢</Button>
+        )
+    } 
+    const BackBtm =() =>{
+        return(
+            <Button onClick={()=>handelBack()} type="default" shape="round" icon={<HomeOutlined />} size="large">Restart</Button>
         )
     } 
     
@@ -74,39 +72,48 @@ const MenuSet = () => {
                 }}>
             <div className="header_box">
                 <img src={spotnext} className="spotnext-logo" alt="spotnext" ></img>
-                <p className="header_inline">SPOT NEXT  </p>
+                <p className="header_inline">SPOT NEXT </p>
             </div>
             {/* select near station */}
-
+            {(!show)?
+            <>
             <div className="select_box">
-                <div className="selector_wrapper">
-                    <div className="wrapper_icon" > <CarOutlined style={{ fontSize: '160%'}} /> </div>
-                    <div className="wrapper_element" > 離你最近的車站： <NearStation station={station} setStation={setStation}/> </div>
-                </div>
-
                 {/* select time interval */}
                 <div className="selector_wrapper">
                     <div className="wrapper_icon" ><HistoryOutlined style={{ fontSize: '160%'}} /></div>
-                    <div className="wrapper_element" > 選擇可接受時間：<TimeSelect setTime={setTime}/></div>
+                    <div className="wrapper_element" > 想要多久之內抵達：<TimeSelect setTime={setTime} inputmode="none" inputReadOnly/></div>
                 </div>
+
+                <div className="selector_wrapper">
+                    <div className="wrapper_icon" > <CarOutlined style={{ fontSize: '160%'}} /> </div>
+                    <div className="wrapper_element" > 離你最近的捷運站： <NearStation station={station} setStation={setStation} inputmode="none" inputReadOnly/> </div>
+                </div>
+
 
                 {/* is transfer? */}
                 <div className="selector_wrapper">
                     <div className="wrapper_icon" ><SwapOutlined style={{ fontSize: '160%'}} /></div>
-                    <div className="wrapper_element" > 決定是否轉乘：<TransferSelect setTransfer={setTransfer}/></div>
+                    <div className="wrapper_element" > 是否接受轉乘：<TransferSelect setTransfer={setTransfer} inputmode="none" inputReadOnly/></div>
                 </div>
 
                 {/* select tag */}
                 <div className="selector_wrapper">
                     <div className="wrapper_icon" ><TagsOutlined style={{ fontSize: '160%'}} /></div>
-                    <div className="wrapper_element" > 請選擇標籤：<TagSelect setTag={setTag}/></div>
+                    <div className="wrapper_element" > 選擇景點分類：<TagSelect setTag={setTag} inputmode="none" inputReadOnly/></div>
                 </div>
     
-                <div className="wrapper_icon">
-                    <div className="check" ><Btm/></div>
-                </div>
+
             </div>
-            {show? <SuggestList returnData={returnData} />:<></>}
+                <div className="wrapper_icon">
+                <div className="check" ><Btm/></div>
+            </div>
+            </>
+            :<><SuggestList returnData={returnData} /> <BackBtm /></>}
+            {/* {show? <SuggestList returnData={returnData} />:<></>} */}
+            {/* {show? <CardList returnData={returnData} />:<></>} */}
+            {/* <div className="card-wrapper">
+                <CardList/>
+            </div> */}
         </div>
     )
 }
